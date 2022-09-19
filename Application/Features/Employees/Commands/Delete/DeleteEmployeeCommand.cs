b2473e479bd56588a4dfbdd04e96d1772c31e6b6
vlikcc <vli.kcc@hotmail.com>
2022-09-19 22:28:02 +0300

    19.09.2022 3. Commit

--- Application/Features/Employees/Commands/Delete/DeleteEmployeeCommand.cs
+++ Application/Features/Employees/Commands/Delete/DeleteEmployeeCommand.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Features.Employees.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,6 +13,26 @@ namespace Application.Features.Employees.Commands.Delete
 {
     public class DeleteEmployeeCommand : IRequest<DeletedEmployeeDto>
     {
+        public int Id { get; set; }
 
+        public class DeleteEmployeeCommandHandler:IRequestHandler<DeleteEmployeeCommand,DeletedEmployeeDto>
+        {
+            private readonly IEmployeeRepository _employeeRepository;
+            private readonly IMapper _mapper;
+
+            public DeleteEmployeeCommandHandler(IEmployeeRepository employeeRepository, IMapper mapper)
+            {
+                _employeeRepository = employeeRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<DeletedEmployeeDto> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+            {
+                Employee? mappedEmployee = _mapper.Map<Employee>(request);
+                Employee deletedEmployee = await _employeeRepository.DeleteAsync(mappedEmployee);  
+                DeletedEmployeeDto result = _mapper.Map<DeletedEmployeeDto>(deletedEmployee);
+                return result;  
+            }
+        }
     }
 }
---
+++ Application/Features/Employees/Commands/Update/UpdateEmployeeCommand.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.Employees.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Employees.Commands.Update
+{
+    public class UpdateEmployeeCommand:IRequest<UpdatedEmployeeDto>
+    {
+        public int Id { get; set; }
+        public string NationalId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string EmailAdress { get; set; }
+        public decimal Salary { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Department { get; set; }
+
+        public class UpdateEmployeeCommandHandler:IRequestHandler<UpdateEmployeeCommand,UpdatedEmployeeDto>
+        {
+            private readonly IEmployeeRepository _employeeRepository;
+            private readonly IMapper _mapper;
+
+            public UpdateEmployeeCommandHandler(IEmployeeRepository employeeRepository, IMapper mapper)
+            {
+                _employeeRepository = employeeRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<UpdatedEmployeeDto> Handle(UpdateEmployeeCommand request, CancellationToken cancellationToken)
+            {
+                Employee? employee = await _employeeRepository.GetAsync(e => e.Id == request.Id);
+                Employee updatedEmployee = _mapper.Map<Employee>(employee);
+                updatedEmployee.FirstName = request.FirstName;
+                updatedEmployee.LastName = request.LastName;
+                updatedEmployee.PhoneNumber = request.PhoneNumber;
+                updatedEmployee.Address = request.Address;
+                updatedEmployee.EmailAdress = request.EmailAdress;
+                updatedEmployee.Salary = request.Salary;
+                updatedEmployee.MaritalStatus = request.MaritalStatus;
+                updatedEmployee.Department = request.Department;
+                await _employeeRepository.UpdateAsync(updatedEmployee);
+                UpdatedEmployeeDto result = _mapper.Map<UpdatedEmployeeDto>(updatedEmployee);
+                return result;
+
+            }
+        }
+    }
+}
